101452d022d7776b3806f06a9a638d240964d57f
Uyiosa <hiskonxeptz@gmail.com>
2023-10-14 15:00:00 +0200

    fix: hide who to follow title and the seem more footer when follow suggestions are hidden

--- src/contentScripts/scripts/tweetProcessor/tweetProcessors.ts
+++ src/contentScripts/scripts/tweetProcessor/tweetProcessors.ts
@@ -33,6 +33,36 @@ export const hidePeopleToFollowSuggestion: TweetProcessor = {
   run(element: HTMLElement, { hidePeopleToFollowSuggestion, showPlaceholderForHiddenAds }) {
     if (!hidePeopleToFollowSuggestion)
       return true
+
+    const hasPeopleToFollow = (node: HTMLElement) => {
+      return (
+        node?.querySelector('[data-testid$="follow"]')
+        && node?.querySelector('[data-testid="UserCell"]')
+      )
+    }
+
+    const hidePeopleToFollowTitleAndSeeMore = (node: HTMLElement) => {
+      const nextSibling = node.nextSibling as HTMLElement
+      const previousSibling = node.previousSibling as HTMLElement | null
+      const previousTwoSibling = node.previousSibling?.previousSibling as HTMLElement | null
+      const previousThreeSiblings = node.previousSibling?.previousSibling?.previousSibling as HTMLElement
+
+      const connectPeopleLink = nextSibling?.querySelector(
+        'a[href^=\'/i/connect_people?user_id=\']',
+      )
+
+      if (previousSibling && previousTwoSibling && connectPeopleLink) {
+        hideTweet(previousThreeSiblings, showPlaceholderForHiddenAds, 'People to follow title')
+        hideTweet(nextSibling, showPlaceholderForHiddenAds, 'See more link')
+      }
+    }
+
+    if (hasPeopleToFollow(element)) {
+      hideTweet(element, showPlaceholderForHiddenAds, 'People to follow')
+      hidePeopleToFollowTitleAndSeeMore(element)
+      return true
+    }
+
     const peopleToFollowSvg
       = 'M17.863 13.44c1.477 1.58 2.366 3.8 2.632 6.46l.11 1.1H3.395l.11-1.1c.266-2.66 1.155-4.88 2.632-6.46C7.627 11.85 9.648 11 12 11s4.373.85 5.863 2.44zM12 2C9.791 2 8 3.79 8 6s1.791 4 4 4 4-1.79 4-4-1.791-4-4-4z'
 
@@ -40,14 +70,6 @@ export const hidePeopleToFollowSuggestion: TweetProcessor = {
     const tweetPhoto = element.querySelector('[data-testid="tweetPhoto"]')?.innerHTML
     if (element.innerHTML.includes(peopleToFollowSvg) && !(tweetText || tweetPhoto)) {
       hideTweet(element, showPlaceholderForHiddenAds, 'People to follow')
-      return true
-    }
-
-    if (
-      element.querySelector('[data-testid$="follow"]')
-      && element.querySelector('[data-testid="UserCell"]')
-    ) {
-      hideTweet(element, true, 'People to follow')
       return true
     }
 
