644f2254307e53896eb6633d5c4aec2f25059c08
Uyiosa <hiskonxeptz@gmail.com>
2023-12-07 16:18:14 +0100

    fix: clean twitter not working on other pages except the home page

--- src/contentScripts/scripts/tweetProcessor/index.ts
+++ src/contentScripts/scripts/tweetProcessor/index.ts
@@ -1,23 +1,30 @@
 import { processTweet } from './processTweet'
 let observerInstance: any
 
-const getTweetsContainer = async (): Promise<any> => {
+const getTweetsContainer = (): Element | null => {
   const targetElement = document.querySelector('[data-testid="primaryColumn"]')
   if (targetElement)
     return targetElement
 
-  await new Promise(resolve => setTimeout(resolve, 500))
-  return getTweetsContainer()
+  return null
 }
 
 function observeTweets(element: Element, options: any): void {
   const observer = new MutationObserver((mutations: MutationRecord[]) => {
     mutations.forEach((mutation: MutationRecord) => {
       mutation.addedNodes.forEach((node: Node) => {
-        const isInTimelineView = (node as Element).closest('[aria-label="Timeline: Your Home Timeline"]')
+        // urls to exclude
+        const excludedPaths = [
+          'followers',
+          'verified_followers',
+          'followers_you_follow',
+          'following',
+        ]
+        const urlPath = location.href.split('/').pop() as string
+        const isExcludedPath = excludedPaths.includes(urlPath)
         const isTweetSection = (node as Element).matches('[data-testid="cellInnerDiv"]')
 
-        if (node.nodeType === Node.ELEMENT_NODE && isTweetSection && isInTimelineView)
+        if (node.nodeType === Node.ELEMENT_NODE && isTweetSection && !isExcludedPath)
           processTweet(node as HTMLElement, options)
       })
     })
@@ -31,9 +38,8 @@ function observeTweets(element: Element, options: any): void {
 }
 
 export async function initializeTweetObserver(options: any): Promise<void> {
-  const tweetsContainer = await getTweetsContainer()
-  tweetsContainer
-    .querySelectorAll('[data-testid="cellInnerDiv"]')
+  const tweetsContainer = getTweetsContainer()
+  tweetsContainer?.querySelectorAll('[data-testid="cellInnerDiv"]')
     .forEach((node: any) => processTweet(node, options))
 
   // continue to monitor for new tweets
--- src/contentScripts/scripts/tweetProcessor/tweetProcessors.ts
+++ src/contentScripts/scripts/tweetProcessor/tweetProcessors.ts
@@ -25,6 +25,17 @@ export const hideTwitterAds: TweetProcessor = {
       hideTweet(element, showPlaceholderForHiddenAds, 'Ad')
       return true
     }
+
+    const hasPlacementTracking = element.querySelector('[data-testid="placementTracking"]')
+    const hasSponsoredIcon = element?.innerHTML.includes('M19.498 3h-15c-1.381 0-2.5 1.12-2.5 2.5v13c0 1.38 1.119 2.5 2.5 2.5h15c1.381 0 2.5-1.12 2.5-2.5v-13c0-1.38-1.119-2.5-2.5-2.5zm-3.502 12h-2v-3.59l-5.293 5.3-1.414-1.42L12.581 10H8.996V8h7v7z')
+
+    const isAnSponsoredTweet = hasPlacementTracking && hasSponsoredIcon
+
+    if (isAnSponsoredTweet && hideTwitterAds) {
+      hideTweet(element, showPlaceholderForHiddenAds, 'Sponsored Tweet')
+      return true
+    }
+
     return false
   },
 }
